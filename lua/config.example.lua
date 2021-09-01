cd0ab2e2d35008a101a77d7cae414569ed97ff8f
Zhengyan Gao <1135326346@qq.com>
2021-09-01 18:01:48 +0800

    Update config.example.lua

--- lua/config.example.lua
+++ lua/config.example.lua
@@ -2,9 +2,16 @@ local M = {}
 M["/bitcoind"] = {
   location = "",
   upstream = "",
+  enabled = true,
 }
 M["/test"] = {
   location = "",
   upstream = "",
+  enabled = true,
+}
+M["/disabled"] = {
+  location = "",
+  upstream = "",
+  enabled = false,
 }
 return M
